9ec976ec64b59a0cf4691d95e54fc2c65c84885e
Dumidu Pabasara <dumidu.pabasara@gmail.com>
2024-07-07 19:41:51 +0530

    bugfix

--- show.c
+++ show.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include "show.h"
 #include "welcomescreen.h" 
+#include "validations.h"
 
 /*
     This function is defined to handle Add theter show to file 
@@ -27,11 +28,19 @@ void addTheatreShow(void){
     /*
         Use to get date
     */ 
+   do{
     // Prompt the user for input
     printf("Enter show date by year-month-date eg:2024-06-20: ");
     // Use scanf to read the input string until newline is encountered
     scanf(" %[^\n]", currentShow.date);
-    printf("You entered: %s\n", currentShow.date);
+    trimSpaces(currentShow.date);
+    if(isValidDate(currentShow.date) == 0) {
+        printf("%s is not a valid date.\n", currentShow.date);
+
+     }
+    } while (isValidDate(currentShow.date) == 0);
+
+      printf("You entered: %s\n", currentShow.date);
 
     /*
         Use to get time
@@ -105,10 +114,18 @@ void displayTheatreSchedule(void){
         Use to get date
     */ 
     char date[15];
+    do{
     // Prompt the user for input
     printf("Enter date by year-month-date eg:2024-06-20: ");
     // Use scanf to read the input string until newline is encountered
     scanf(" %[^\n]", date);
+    trimSpaces(date);
+
+    if(isValidDate(date) == 0) {
+        printf("%s is not a valid date.\n", date);
+    }
+    } while (isValidDate(date) == 0);
+
     printf("You entered: %s\n", date); 
     // ------------------------------------- to do regex validate
 
