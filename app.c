af6d790d7fef70125580e8dcb8b59d830e570686
ChandimaJayamina <166395933+ChandimaJayamina@users.noreply.github.com>
2024-07-12 21:38:50 +0530

    Update app.c

--- app.c
+++ app.c
@@ -14,11 +14,8 @@ int main(void) {
 
 
 /* 
-  need PCRE lib for regex - 'sudo apt-get update
-sudo apt-get install libpcre3 libpcre3-dev' | mac 'brew install pcre
-'
   compile steps: 
-    gcc -o app app.c welcomescreen.c show.c  -I/opt/homebrew/Cellar/pcre/8.45/include -L/opt/homebrew/Cellar/pcre/8.45/lib -lpcre
+    gcc -o app app.c welcomescreen.c show.c validations.c 
   run : ./app
   quit ctrl+c
 */
