77e8495806f0a3d840fdbca580132726c4ec9a73
ChandimaJayamina <166395933+ChandimaJayamina@users.noreply.github.com>
2024-07-13 23:14:58 +0530

    Update app.c

--- app.c
+++ app.c
@@ -1,19 +1,5 @@
 #include <stdio.h>
 #include "welcomescreen.h"
-#include <time.h>
-
-void someFunction() {
-    time_t now = time(NULL);
-    struct tm *local = localtime(&now);
-
-    if (local != NULL) {
-        printf("Current local time: %s", asctime(local));
-    } else {
-        printf("Failed to get local time.\n");
-    }
-
-    // Code that uses the 'local' variable or other logic
-}
 
 int main(void)
 {
